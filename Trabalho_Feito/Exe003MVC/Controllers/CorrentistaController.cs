33f454b8f656b607b5e54a71da33df7e92250318
rockmandr <rodrigoyukimide00@gmail.com>
2020-05-11 18:41:15 -0300

    MVC

---
+++ Trabalho_Feito/Exe003MVC/Controllers/CorrentistaController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Exe003MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exe003MVC.Controllers
+{
+    public class CorrentistaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            var model = new Correntista();
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public IActionResult Cadastrar(Correntista users, string nome, DateTime data, string cpf, int rg,
+        int cep, string rua, string cidade, string estado, string pais, float salario, float patrimonio,
+        string empresaEmQueTrabalha, int tempoNoEmprego, string cargo, string numeroDaConta)
+        {
+            //Criamos a variavél idadeValida que armazena o 
+            //resultado da validação da regra de negócio da idade 
+            //ser maior ou igual a 16
+            if (users.ValidarIdade() == false)
+            {
+                ModelState.AddModelError("DataNascimento", "É necessário a idade maior ou igual a 16 anos!");
+            }
+            else if (users.ValidaCpf(cpf) == false)
+            {
+                ModelState.AddModelError("Cpf", "É necessário CPF valído!");
+            }
+            else if (users.ValidaPais() == false)
+            {
+                ModelState.AddModelError("Pais", "O(a) Correntista deve morar no Brasil!");
+            }
+
+            /*
+            else if (users.CodigoCorrentista(numeroDaConta) != "")
+            {
+                ModelState.AddModelError("NumeroDaConta", "'-'");
+            }
+            */
+
+            return View(users);
+        }
+
+
+        [HttpPost()]
+        public IActionResult Details(string nome, DateTime data, string cpf, int rg,
+        int cep, string rua, string cidade, string estado, string pais, float salario, float patrimonio,
+        string empresaEmQueTrabalha, int tempoNoEmprego, string cargo, string numeroDaConta)
+        {
+
+            Correntista users = new Correntista();
+
+            users.Nome = nome;
+            users.DataNascimento = data;
+            users.Cpf = cpf;
+            users.Rg = rg;
+
+            users.Cep = cep;
+            users.Rua = rua;
+            users.Cidade = cidade;
+            users.Estado = estado;
+            users.Pais = pais;
+
+            users.Salario = salario;
+            users.Patrimonio = patrimonio;
+
+            users.EmpresaEmQueTrabalha = empresaEmQueTrabalha;
+            users.TempoNoEmprego = tempoNoEmprego;
+            users.Cargo = cargo;
+            users.NumeroDaConta = numeroDaConta;
+
+            users.CodigoCorrentista(numeroDaConta);
+
+            return View(users);
+
+        }
+    }
+}
---
+++ Trabalho_Feito/Exe003MVC/Models/Correntista.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exe003MVC.Models
+{
+    public class Correntista
+    {
+        public string Nome { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Data de Nascimento é obrigatório")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Data de Nascimento")]
+        public DateTime DataNascimento { get; set; }
+
+
+        public string Cpf { get; set; }
+        public int Rg { get; set; }
+
+        //Dados de endereço
+        public int Cep { get; set; }
+        public string Rua { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Pais { get; set; }
+
+        //Dados financeiros
+        public float Salario { get; set; }
+        public float Patrimonio { get; set; }
+
+        //Dados empregador
+        public string EmpresaEmQueTrabalha { get; set; }
+        public int TempoNoEmprego { get; set; }
+        public string Cargo { get; set; }
+
+        //Conta do Correntista
+        public string NumeroDaConta { get; set; }
+        //public float s { get; set; }
+
+        public bool ValidarIdade()
+        {
+            TimeSpan tempo = (DateTime.Now - DataNascimento);
+            var idade = tempo.TotalDays / 365;
+
+            return idade >= 16;
+        }
+
+        public bool ValidaCpf(string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+
+            int soma;
+            int resto;
+
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+
+            if (cpf.Length != 11)
+            {
+                return false;
+            }
+            tempCpf = cpf.Substring(0, 9);
+
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                soma += int.Parse(tempCpf[i].ToString()) * (multiplicador1[i]);
+            }
+            resto = soma % 11;
+
+            if (resto < 2)
+            {
+                resto = 0;
+            }
+            else
+            {
+                resto = 11 - resto;
+            }
+
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            int soma2 = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                soma2 += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            }
+
+            resto = soma2 % 11;
+
+            if (resto < 2)
+            {
+                resto = 0;
+            }
+            else
+            {
+                resto = 11 - resto;
+            }
+
+            digito = digito + resto.ToString();
+            return cpf.EndsWith(digito);
+        }
+
+        public bool ValidaPais()
+        {
+            if (Pais != "Brasil" && Pais != "Brazil")
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public string CodigoCorrentista(string codi)
+        {
+            var hoje = DateTime.Now;
+            string hoje_str = hoje.ToString("dd/MM/yyyy");
+            string estraindoUltimosNumeros = hoje_str.Substring(6, 2);
+
+
+            var data = DataNascimento;
+            string data_str = data.ToString("dd/MM/yyyy");
+            string estraindoUltimosData = data_str.Substring(8, 2);
+
+
+            var cpf = Cpf;
+            string estraindoUltimosCPF = cpf.Substring(8, 3);
+
+
+            string estraindoDia = hoje_str.Substring(0, 2);
+
+            string codigo = estraindoUltimosNumeros + estraindoUltimosData + estraindoUltimosCPF + '-' + estraindoDia;
+
+            NumeroDaConta = codigo;
+
+            codi = NumeroDaConta;
+
+            return codi;
+        }
+
+        /*
+        private float SaldoCorrentista(string codigo, float saldo)
+        {
+            Codigo = codigo;
+            Conta = 0;
+
+            return saldo;
+        }
+        */
+    }
+}
